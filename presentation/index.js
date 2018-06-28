49142eef161c3aa768fca7c2b4e5a4b804198053
Jen Luker <jluker@deseretdigital.com>
2018-06-28 03:06:11 -0600

    Changing title slide

--- presentation/index.js
+++ presentation/index.js
@@ -1,11 +1,25 @@
 // Import React
 import React from "react";
 import { AboutMe, Formidable } from "./aboutme";
-import { A11yIntro, WhatIsA11y, WhatIsDisability, TypesOfDisability, Why, WhyBusiness1, WhyBusiness2, WhyBusiness3 } from "./aboutA11y";
+import {
+  A11yIntro,
+  WhatIsA11y,
+  WhatIsDisability,
+  TypesOfDisability,
+  Why,
+  WhyBusiness1,
+  WhyBusiness2,
+  WhyBusiness3,
+} from "./aboutA11y";
 import { GettingStartedIntro, Axe, DesignTools, DevTools } from "./gettingStarted";
 import { TestYourSiteIntro } from "./testYourSite";
 import { BreakdownIntro, Checklists, StartNow, ByComponent, ByPage } from "./breakdown";
-import { RealWorldApplication, ButtonLinkDiv, DesignAffectsCodeCheckbox, DesignAffectsCodeMultiselect } from "./a11yDecisions";
+import {
+  RealWorldApplication,
+  ButtonLinkDiv,
+  DesignAffectsCodeCheckbox,
+  DesignAffectsCodeMultiselect,
+} from "./a11yDecisions";
 // Import Spectacle Core tags
 import {
   Appear,
@@ -21,7 +35,7 @@ import {
   Notes,
   Quote,
   Slide,
-  Text
+  Text,
 } from "spectacle";
 
 // Import image preloader util
@@ -32,8 +46,7 @@ import createTheme from "spectacle/lib/themes/default";
 import styles from "./styles";
 const images = {
   JenDevDesk: require("../assets/aboutme/JensDesk.jpg"),
-  JenDeskColor: require("../assets/aboutme/JensDeskColor.jpg")
-
+  JenDeskColor: require("../assets/aboutme/JensDeskColor.jpg"),
 };
 preloader(images);
 
@@ -47,11 +60,11 @@ const themeColors = {
   dktertiary: "deepskyblue",
   quartenary: "#FAFAFA",
   faded: "#CCCCCC",
-  background: "linear-gradient(#F8FAFA, #D8DADA)"
+  background: "linear-gradient(#F8FAFA, #D8DADA)",
 };
 
 const theme = createTheme(themeColors, {
-  primary: "Raleway"
+  primary: "Raleway",
 });
 
 export default class Presentation extends React.Component {
@@ -59,44 +72,20 @@ export default class Presentation extends React.Component {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         {/* Title Screen */}
-        <Slide
-          bgColor="faded"
-          bgImage={images.JenDevDesk}
-          bgDarken={0.7}
-        >
+        <Slide bgColor="faded" bgImage={images.JenDevDesk} bgDarken={0.7}>
           <Notes>
-						As speakers, we do our best, but sometimes slides are
-						just too difficult to read. You can follow along at
-						a11y-sprint-filler.surge.sh
+            As speakers, we do our best, but sometimes slides are just too difficult to read. You can follow along at
+            be-a-react-native-a11y.surge.sh
           </Notes>
           <Layout style={styles.flexCol}>
             <Fill style={styles.spacePlease}>
-              <Heading
-                caps
-                size={1}
-                textColor="primary"
-                style={{ textShadow: "1px 1px" + " 6px #111" }}
-              >
-								Accessibility
-              </Heading>
-              <Heading
-                margin="10px 0 0"
-                textColor="dktertiary"
-                size={3}
-                bold
-                style={{ textShadow: "1px 1px" + " 6px #111" }}
-              >
-								the sprint-filler way
+              <Heading caps size={2} textColor="primary" style={{ textShadow: "1px 1px" + " 6px #111" }}>
+                Be a React Native A11y
               </Heading>
             </Fill>
             <Fill style={styles.spacePlease}>
-              <Link
-                textColor="dktertiary"
-                style={styles.smallText}
-                href="http://a11y-sprint-filler.surge.sh"
-              >
-								Follow along with the slides:
-								http://a11y-sprint-filler.surge.sh
+              <Link textColor="dktertiary" style={styles.smallText} href="http://a11y-sprint-filler.surge.sh">
+                Follow along with the slides: http://be-a-react-native-a11y.surge.sh
               </Link>
               {/* <br />
 							<Link
@@ -145,19 +134,17 @@ export default class Presentation extends React.Component {
         <RealWorldApplication />
         <ButtonLinkDiv />
         <DesignAffectsCodeCheckbox />
-				<DesignAffectsCodeMultiselect />
+        <DesignAffectsCodeMultiselect />
 
         {/* Conclusion */}
-				<Slide
-				bgColor="#000000"
-        style={{ width: "100%", position: "relative" }}>
-					<Text textColor="dktertiary" style={[styles.largeText, styles.littleSpacePlease]}>
-						Accessibility isn't for just a select few. It's for everybody. It's for you.
-					</Text>
-					<Heading size={2} textColor="primary" style={styles.littleSpacePlease}>
-						Thank you
-					</Heading>
-				</Slide>
+        <Slide bgColor="#000000" style={{ width: "100%", position: "relative" }}>
+          <Text textColor="dktertiary" style={[styles.largeText, styles.littleSpacePlease]}>
+            Accessibility isn't for just a select few. It's for everybody. It's for you.
+          </Text>
+          <Heading size={2} textColor="primary" style={styles.littleSpacePlease}>
+            Thank you
+          </Heading>
+        </Slide>
       </Deck>
     );
   }
